3c8b3ceda9cf9568957eee4c7750bd41c7ddf12f
Chris A. Long <long.chris.619@gmail.com>
2023-03-13 00:00:38 -0700

    Fixed typo in comment describing exercise.

--- ch1/exercise1-9.c
+++ ch1/exercise1-9.c
@@ -3,7 +3,7 @@
 /*
     Exercise 1-9
     Write a program to copy its input to its output, replacing each string of
-    one or moe blanks by a single blank.
+    one or more blanks by a single blank.
 */
 
 main()
